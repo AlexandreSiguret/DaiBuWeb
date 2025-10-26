70fa046ed5ed7b6cbd9b73b0e611df9e889e44f3
Alexandre Siguret <alexandresiguret@MacBook-Pro-de-Alexandre.local>
2025-10-26 21:05:42 +0100

    Changement couleur CTA

--- components/CTAANDROID.vue
+++ components/CTAANDROID.vue
@@ -12,7 +12,7 @@ withDefaults(defineProps<Props>(), {
   <NuxtLink
     href="https://play.google.com/store/apps/details?id=com.yourmoneyclockyeah.moneymoneymoney&hl=fr&gl=GB"
     target="_blank"
-    :class="['rounded font-bold text-center text-white bg-[#95E3B7] rounded-lg px-4 py-2 hover:bg-[#8ad4aa]',
+    :class="['rounded font-bold text-center text-darkbg bg-[#95E3B7] rounded-lg px-4 py-2 hover:bg-[#8ad4aa]',
       { 'lg:text-3xl lg:px-10 lg:py-4': size === 'xl' }]"
   >
     Télécharger sur Play Store
--- components/CTAIOS.vue
+++ components/CTAIOS.vue
@@ -12,7 +12,7 @@ withDefaults(defineProps<Props>(), {
   <NuxtLink
     href="https://apps.apple.com/fr/app/daibu-daily-budget/id6740541346"
     target="_blank"
-    :class="['rounded font-bold text-center text-white bg-[#95E3B7] rounded-lg px-4 py-2 hover:bg-[#8ad4aa]',
+    :class="['rounded font-bold text-center text-darkbg bg-[#95E3B7] rounded-lg px-4 py-2 hover:bg-[#8ad4aa]',
       { 'lg:text-3xl lg:px-10 lg:py-4': size === 'xl' }]"
   >
     Télécharger sur App Store
