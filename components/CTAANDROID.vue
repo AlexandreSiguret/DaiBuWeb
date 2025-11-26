83ce12e496d50ade00d530076d287822a8f1b9a4
Alexandre Siguret <alexandre.siguret@gmail.com>
2025-11-26 13:05:01 +0100

    Changement lien Store

--- components/CTAANDROID.vue
+++ components/CTAANDROID.vue
@@ -10,7 +10,7 @@ withDefaults(defineProps<Props>(), {
 
 <template>
   <NuxtLink
-    href="https://play.google.com/store/apps/details?id=com.yourmoneyclockyeah.moneymoneymoney&hl=fr&gl=GB"
+    href="https://play.google.com/store/apps/details?id=com.yourmoneyclockyeah.moneymoneymoney"
     target="_blank"
     :class="['rounded font-bold text-center text-darkbg bg-[#95E3B7] rounded-lg px-4 py-2 hover:bg-[#8ad4aa]',
       { 'lg:text-3xl lg:px-10 lg:py-4': size === 'xl' }]"
